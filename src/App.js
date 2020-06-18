441b8ac4b8f569ede066e9100d57751717c269ce
chudoklates <franczalko@gmail.com>
2020-06-18 12:13:53 +0200

    Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, NavLink, Redirect } from 'react-router-dom'
 import './App.css';
+import Task from './components/Task'
+import Search from './components/Search'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header style={{ display: 'flex', justifyContent: 'space-around', marginTop: '40px' }}>
+        <NavLink
+          to="/"
+          exact
         >
-          Learn React
-        </a>
+          List
+          </NavLink>
+        <NavLink
+          to="/search"
+        >
+          Search
+        </NavLink>
       </header>
+      <Switch>
+        <Route
+          exact
+          path="/"
+        >
+          <Task />
+        </Route>
+        <Route
+          path="/search"
+        >
+          <Search />
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/constants/_typeList.js
@@ -0,0 +1,10 @@
+export default [
+  { type: 'E-LEARNING', count: 14 },
+  { type: 'ARTICLE', count: 2 },
+  { type: 'E-LEARNING', count: 30 },
+  { type: 'VIDEO', count: 7 },
+  { type: 'TOOL', count: 1 },
+  { type: 'VIDEO', count: 0 },
+  { type: 'ARTICLE', count: 45 },
+  { type: 'E-LEARNING', count: 3 }
+]
